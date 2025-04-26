38d1a1fde309bf9a2e692b2cb6d9b8c23edaae93
Oscar Antia <122615920+devoscar-antia@users.noreply.github.com>
2025-04-26 00:34:32 -0500

    feat: se agrega liberia syncfusion y se crea la view del temporizador

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:totp_unix/views/temporizador_view.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const UTOTPApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class UTOTPApp extends StatelessWidget {
+  const UTOTPApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Unix 12',
+      title: 'Unix',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text('$_counter')],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: TemporizadorView(),
     );
   }
 }
